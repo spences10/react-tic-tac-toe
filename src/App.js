fa897bc432f1c3f9f255181f66c1af91e89f2cb2
spences10 <spences10apps@gmail.com>
2017-04-01 16:08:12 +0100

    :alarm_clock:Implementing Time Travel:alarm_clock:

--- src/App.js
+++ src/App.js
@@ -10,13 +10,21 @@ class App extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      xIsNext: true
+      xIsNext: true,
+      stepNumber: 0
     }
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) ? false : true
+    })
   }
 
   handleClick(i) {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const squares = current.squares.slice()
     if (calculateWinner(squares) || squares[i]) {
       return
@@ -27,12 +35,13 @@ class App extends React.Component {
         squares: squares
       }]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
     })
   }
 
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
 
     let status
@@ -45,7 +54,7 @@ class App extends React.Component {
     const moves = history.map((step, move) => {
       const desc = move ? 'Move #' + move : 'Game start'
       return (
-        <li>
+        <li key={move}>
           <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
         </li>
       )
