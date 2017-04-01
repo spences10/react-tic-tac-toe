79252bf7fc848fc028d4b8f361bddecc7929cea6
spences10 <spences10apps@gmail.com>
2017-04-01 16:33:02 +0100

    :alarm_clock:finishe time travel:alarm_clock:

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[this.state.stepNumber]
     const squares = current.squares.slice()
     if (calculateWinner(squares) || squares[i]) {
