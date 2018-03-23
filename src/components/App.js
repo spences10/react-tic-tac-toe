29d0a319da498023d4daedac17f36328692aa32f
spences10 <spences10apps@gmail.com>
2018-03-23 06:48:23 +0000

    style basic app

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,54 @@
 import React, { Component } from 'react'
+import styled, { keyframes, ThemeProvider } from 'styled-components'
+
+import { theme1, theme2, Button } from '../theme/globalStyle'
+import ThemeSelect from './ThemeSelect'
+
 import Board from './Board'
 import { calculateWinner } from '../helpers'
+
 import logo from '../logo.svg'
 import './App.css'
+
+const AppWrapper = styled.div`
+  text-align: center;
+`
+
+const AppHeader = styled.div`
+  height: 12rem;
+  padding: 1rem;
+  color: ${props => props.theme.dark};
+  background-color: ${props => props.theme.primary};
+`
+
+const AppTitle = styled.h1`
+  font-weight: 900;
+`
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const AppLogo = styled.img`
+  animation: ${rotate360} infinite 2s linear;
+  height: 80px;
+  &:hover {
+    animation: ${rotate360} infinite 0.5s linear;
+  }
+`
+
+const AppIntro = styled.p`
+  color: ${props => props.theme.dark};
+  font-size: large;
+  code {
+    font-size: 1.3rem;
+  }
+`
 
 class App extends Component {
   state = {
@@ -12,7 +58,8 @@ class App extends Component {
       }
     ],
     xIsNext: true,
-    stepNumber: 0
+    stepNumber: 0,
+    theme: theme1
   }
 
   jumpTo(step) {
@@ -44,6 +91,12 @@ class App extends Component {
     })
   }
 
+  handleThemeChange = e => {
+    let theme = e.target.value
+    theme === 'theme1' ? (theme = theme1) : (theme = theme2)
+    this.setState({ theme })
+  }
+
   render() {
     const history = this.state.history
     const current = history[this.state.stepNumber]
@@ -68,30 +121,35 @@ class App extends Component {
     })
 
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React tic-tac-toe</h2>
-        </div>
-        <p className="App-intro">
-          Follow along of the tutorial{' '}
-          <code>
-            https://facebook.github.io/react/tutorial/tutorial.html
-          </code>.
-        </p>
-        <div className="game">
-          <div className="game-board">
-            <Board
-              squares={current.squares}
-              onClick={i => this.handleClick(i)}
-            />
+      <ThemeProvider theme={this.state.theme}>
+        <AppWrapper>
+          <AppHeader>
+            <AppLogo src={logo} className="App-logo" alt="logo" />
+            <AppTitle>React tic-tac-toe</AppTitle>
+          </AppHeader>
+          <AppIntro>
+            Follow along of the tutorial{' '}
+            <underline>
+              <code>
+                https://facebook.github.io/react/tutorial/tutorial.html
+              </code>.
+            </underline>
+          </AppIntro>
+          <div className="game">
+            <div className="game-board">
+              <Board
+                squares={current.squares}
+                onClick={i => this.handleClick(i)}
+              />
+            </div>
+            <div className="game-info">
+              <div>{status}</div>
+              <ol>{moves}</ol>
+            </div>
           </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol>{moves}</ol>
-          </div>
-        </div>
-      </div>
+          <ThemeSelect handleThemeChange={this.handleThemeChange} />
+        </AppWrapper>
+      </ThemeProvider>
     )
   }
 }
