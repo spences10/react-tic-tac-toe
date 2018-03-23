b99f42eca01caa7aa35344ee7dcb3b32c25460f9
spences10 <spences10apps@gmail.com>
2018-03-23 19:33:00 +0000

    remove board css

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import Square from './Square'
-import './Board.css'
+
+const BoardWrapper = styled.div`
+  margin: 1rem;
+`
+
+const BoardRow = styled.div`
+  &:after {
+    clear: both;
+    content: '';
+    display: table;
+  }
+`
 
 class Board extends React.Component {
   renderSquare(i) {
@@ -14,23 +27,23 @@ class Board extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className="board-row">
+      <BoardWrapper>
+        <BoardRow>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
+        </BoardRow>
+        <BoardRow>
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
+        </BoardRow>
+        <BoardRow>
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-        </div>
-      </div>
+        </BoardRow>
+      </BoardWrapper>
     )
   }
 }
