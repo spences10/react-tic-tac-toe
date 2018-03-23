b072d8843815db7ca0b3846fe8983ec315ceef3a
spences10 <spences10apps@gmail.com>
2018-03-23 19:36:06 +0000

    remove suqare css

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,23 @@
 import React from 'react'
-import './Square.css'
+import styled from 'styled-components'
+
+const StyledSquare = styled.button`
+  background: #fff;
+  border: 2px solid #999;
+  float: left;
+  font-size: 48px;
+  font-weight: bold;
+  line-height: 34px;
+  height: 50px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  width: 50px;
+  &:focus {
+    outline: none;
+  }
+`
 
 class Square extends React.Component {
   constructor() {
@@ -10,9 +28,9 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
+      <StyledSquare onClick={() => this.props.onClick()}>
         {this.props.value}
-      </button>
+      </StyledSquare>
     )
   }
 }
