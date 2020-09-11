7a72203dd153926b98ba4c3c7d0b79e65cb7058e
spences10 <spences10apps@gmail.com>
2020-09-11 20:03:40 +0100

    remove gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,14 @@
+import { Fragment } from 'preact'
 import React from 'react'
 import { Game } from '../components/game'
 
 export default () => {
-  return <Game />
+  return (
+    <Fragment>
+      <Helmet>
+        <link rel="stylesheet" href="/styles/src/pages/index.css" />
+      </Helmet>
+      <Game />
+    </Fragment>
+  )
 }
